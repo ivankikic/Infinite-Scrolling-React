c5b56ed526a50fa16ba4b7e2b9f1a55c926874f1
Ivan Kikic <ivankikic49@gmail.com>
2023-10-07 13:27:15 +0200

    get book from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import useBookSearch from "./useBookSearch";
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
   return (
-    <div className="App">
-      <p>Welcome</p>
-    </div>
+    <>
+      <input type="text" value={query} onChange={handleSearch} />
+      {books && books.map((book, index) => {
+        return <div key={index}>{book}</div>
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
+    </>
   );
 }
 
